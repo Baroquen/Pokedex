becd05a4116b97d3805373b5aacac454e5b2c9d5
M. Becky Latta <Becky.Latta@gmail.com>
2023-01-09 11:18:29 -0600

    updated the searchbar to have a disabled option and updated the pokedex to filter based on the searchbar results

--- src/features/history/SearchBar.tsx
+++ src/features/history/SearchBar.tsx
@@ -2,17 +2,23 @@ import { FC } from "react";
 import Container from "react-bootstrap/Container";
 
 interface searchBarProps {
+  disabled: boolean;
   onChange: (n: string) => void;
 }
 
-export const SearchBar: FC<searchBarProps> = ({ onChange }) => {
+export const SearchBar: FC<searchBarProps> = ({ disabled, onChange }) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value);
   };
 
   return (
     <Container>
-      <input type="search" placeholder="Pikachu" onChange={handleChange} />
+      <input
+        type="search"
+        placeholder="Pikachu..."
+        onChange={handleChange}
+        disabled={disabled}
+      />
     </Container>
   );
 };
