9fccf199dbef3e80b948f3fa92bd4e7f49491bf2
M. Becky Latta <Becky.Latta@gmail.com>
2023-01-09 11:19:24 -0600

    Added a skeleton to the PokemonCard component

--- src/features/pokedex/PokemonCard.tsx
+++ src/features/pokedex/PokemonCard.tsx
@@ -1,19 +1,34 @@
+import { FC } from "react";
 import Card from "react-bootstrap/Card";
 import { useGetPokemonByNameQuery } from "./pokedexApi";
+import { If, Then, Else } from "react-if";
+import Skeleton from "react-loading-skeleton";
 
 interface PokemonProps {
-    name: string
+  name: string;
 }
 
-export function PokemonCard({name} : PokemonProps) {
-    const { data } = useGetPokemonByNameQuery(name);
+export const PokemonCard: FC<PokemonProps> = ({ name }: PokemonProps) => {
+  const { data, isLoading, isFetching } = useGetPokemonByNameQuery(name);
 
   return (
-    <Card key={name}>
-        <Card.Img variant="top" src={data?.sprites.front_default}/>
-      <Card.Body>
-        <Card.Title className="text-center">{name}</Card.Title>
-      </Card.Body>
-    </Card>
+    <If condition={isLoading || isFetching}>
+      <Then>
+        <Card>
+          <Card.Img />
+          <Card.Body>
+            <Skeleton height={118} width={210} enableAnimation />
+          </Card.Body>
+        </Card>
+      </Then>
+      <Else>
+        <Card>
+          <Card.Img variant="top" src={data?.sprites.front_default} />
+          <Card.Body>
+            <Card.Title className="text-center">{data?.name}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Else>
+    </If>
   );
-}
+};
